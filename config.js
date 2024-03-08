df39da04d8a6cf10b0ea3c916e6cc821e355f7d0
Priyans4475 <priyanshukunar007@gmail.com>
2024-03-09 03:31:51 +0530

    fix: .env

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const mysql=require('mysql')
 require("dotenv").config();
-const con=mysql.createConnection({
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE
-})
+const con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
 
 con.connect((err)=>{
     if(err)
--- routes/user.js
+++ routes/user.js
@@ -97,3 +97,4 @@ router.post('/signin', (req, res) => {
 
 
 module.exports=router;
+
