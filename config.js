9700d1a30ad42037dc5f15a401870e3193051aa4
Priyans4475 <priyanshukunar007@gmail.com>
2024-03-09 03:15:07 +0530

    env update

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const mysql=require('mysql')
+require("dotenv").config();
 const con=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:"",
-    database:"financemanagement"
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE
 })
 
 con.connect((err)=>{
